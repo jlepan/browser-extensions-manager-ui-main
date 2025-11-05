a23ebd20a3b9c437226de3a2e568a0bcd3f3b377
jlepan <juliette.lepan@hotmail.fr>
2025-11-05 16:43:10 +0100

    Initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  printWidth: 500, // Ne coupe pas les lignes
+  semi: true, // Utilise des points-virgules à la fin de chaque ligne
+  tabWidth: 2, // Utilise une indentation de 2 espaces
+  trailingComma: "es5", // Ajoute des virgules finales lorsque c'est valide en ES5 (objets, tableaux, etc.)
+  endOfLine: "auto", // Maintient les fins de ligne existantes
+};
